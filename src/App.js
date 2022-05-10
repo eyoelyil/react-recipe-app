81d14330bad6547ff85324b8e199e0c1f891e163
Mulate Eyoel <s2200125@87-92-214-203.rev.dnainternet.fi>
2022-05-11 00:21:13 +0300

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <h1>Food Recipe App</h1>
+      <Header />
     </div>
   );
 }
